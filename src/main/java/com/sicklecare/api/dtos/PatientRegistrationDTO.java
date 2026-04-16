54fb7c700d4e6518a58333126180899dd62bc142
YoungIT237 <ityoung742@gmail.com>
2026-04-16 09:37:24 +0100

    Implementation of Data Transmission Object for Patient Registration

---
+++ src/main/java/com/sicklecare/api/dtos/PatientRegistrationDTO.java
@@ -0,0 +1,41 @@
+package com.sicklecare.api.dtos;
+
+import com.sicklecare.api.models.Role;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class PatientRegistrationDTO {
+
+    @NotBlank(message = "Username is require !")
+    @Size(min = 4, max = 20, message = "Username will have between 4 and 20 characters !")
+    private String username;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is require !")
+    private String email;
+
+    @NotBlank(message = "Password is require !")
+    @Size(min = 8, message = "Password will have 8 characters or more !")
+    private String password;
+
+    private Role role;
+    private Boolean active = true;
+    private Boolean validated = false;
+
+    private String firstname;
+    private String lastname;
+    private Boolean sex;
+    @Past(message = "Birth date should be past !")
+    private LocalDate birthDate;
+    private String genotype;
+    private String bloodType;
+    @Positive(message = "Weight should be positive !")
+    private Double weight;
+    private String region;
+    private String city;
+
+}
