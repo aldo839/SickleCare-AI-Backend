fd91988f377d88324b8b001fd61d4a16af499a25
YoungIT237 <ityoung742@gmail.com>
2026-04-16 09:16:10 +0100

    Initialisation of User repository with a method like findByUsername, findByEmail, existsByUsername, existsByEmail

---
+++ src/main/java/com/sicklecare/api/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.sicklecare.api.repository;
+
+import com.sicklecare.api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+}
