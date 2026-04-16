d35a90b48280c71066bc4afa06746b525c01d735
YoungIT237 <ityoung742@gmail.com>
2026-04-16 17:36:47 +0100

    Modification of our patient validation repository to add method findByCode that will be useful for patient account activation

--- src/main/java/com/sicklecare/api/repository/PatientValidationRepository.java
+++ src/main/java/com/sicklecare/api/repository/PatientValidationRepository.java
@@ -3,5 +3,9 @@ package com.sicklecare.api.repository;
 import com.sicklecare.api.models.PatientValidation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientValidationRepository extends JpaRepository<PatientValidation, Long> {
+
+    Optional<PatientValidation> findByCode(String code);
 }
