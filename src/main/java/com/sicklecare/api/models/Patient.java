f8207925dcbe6ed8be089e6339908c838c1f6040
YoungIT237 <ityoung742@gmail.com>
2026-04-16 08:29:31 +0100

    Extension of User entity with data specific to Sickle cell as Patient entity

---
+++ src/main/java/com/sicklecare/api/models/Patient.java
@@ -0,0 +1,33 @@
+package com.sicklecare.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "patients")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Patient extends User {
+
+    private String firstname;
+    private String lastname;
+    private Boolean sex;
+    @Column(name = "birth_date")
+    @Past
+    private LocalDate birthDate;
+    @Column(name = "blood_type")
+    private String bloodType;
+    private String genotype;
+    private Double weight;
+    private String region;
+    private String city;
+
+}
