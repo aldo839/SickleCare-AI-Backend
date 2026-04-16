5a9f46230d25c22f3a780040a61550d27e8b0789
YoungIT237 <ityoung742@gmail.com>
2026-04-16 09:42:39 +0100

    Implementation of Patient Controller

---
+++ src/main/java/com/sicklecare/api/controllers/PatientController.java
@@ -0,0 +1,61 @@
+package com.sicklecare.api.controllers;
+
+import com.sicklecare.api.dtos.PatientRegistrationDTO;
+import com.sicklecare.api.dtos.PatientResponseDTO;
+import com.sicklecare.api.dtos.PatientUpdateDTO;
+import com.sicklecare.api.models.Patient;
+import com.sicklecare.api.repository.PatientRepository;
+import com.sicklecare.api.services.PatientService;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+@CrossOrigin(origins = "*")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
+
+        return ResponseEntity.ok(patientService.getAllPatients());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> getPatientById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(patientService.getPatientByID(id));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<PatientResponseDTO> registerPatient(@Valid @RequestBody PatientRegistrationDTO patientRegistrationDTO) throws MessagingException, UnsupportedEncodingException {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientService.registerPatient(patientRegistrationDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable Long id, @Valid @RequestBody PatientUpdateDTO patientUpdateDTO){
+
+        return ResponseEntity.ok(patientService.updatePatient(id, patientUpdateDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePatient(@PathVariable Long id){
+
+        patientService.deletePatient(id);
+        return ResponseEntity.ok("Patient deleted Successfully");
+    }
+
+}
---
+++ src/main/java/com/sicklecare/api/services/PatientService.java
@@ -0,0 +1,134 @@
+package com.sicklecare.api.services;
+
+import com.sicklecare.api.dtos.PatientRegistrationDTO;
+import com.sicklecare.api.dtos.PatientResponseDTO;
+import com.sicklecare.api.dtos.PatientUpdateDTO;
+import com.sicklecare.api.exceptions.ResourceNotFoundException;
+import com.sicklecare.api.exceptions.UserAlreadyExistsException;
+import com.sicklecare.api.models.Patient;
+import com.sicklecare.api.models.Role;
+import com.sicklecare.api.repository.PatientRepository;
+import com.sicklecare.api.repository.UserRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+
+    private final UserRepository userRepository;
+
+    private final PatientRepository patientRepository;
+
+    private final PatientMapperService patientMapperService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final PatientValidationService patientValidationService;
+
+    public PatientResponseDTO registerPatient(PatientRegistrationDTO dto) {
+
+        // Existence verification
+        if (userRepository.existsByUsername(dto.getUsername())){
+            System.out.println("Error : " + dto.getUsername() + "exist");
+            throw new UserAlreadyExistsException("This user name already exists!");
+        }
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            System.out.println("Error : " + dto.getEmail() + "exist");
+            throw new UserAlreadyExistsException("Email already exists!");
+        }
+
+        Patient patient = new Patient();
+        patient.setUsername(dto.getUsername());
+        patient.setEmail(dto.getEmail());
+        patient.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+        // Mapping of all fields
+        patient.setFirstname(dto.getFirstname());
+        patient.setLastname(dto.getLastname());
+        patient.setSex(dto.getSex());
+        patient.setGenotype(dto.getGenotype());
+        patient.setBirthDate(dto.getBirthDate());
+        patient.setBloodType(dto.getBloodType());
+        patient.setWeight(dto.getWeight());
+        patient.setRegion(dto.getRegion());
+        patient.setCity(dto.getCity());
+
+        patient.setRole(Role.PATIENT);
+        patient.setActive(true);
+        patient.setValidated(false);
+
+        // Save patient
+        Patient savedPatient = patientRepository.save(patient);
+
+        // validation
+        try {
+            patientValidationService.save(savedPatient);
+        } catch (Exception e) {
+            System.err.println("Validation error : " + e.getMessage());
+        }
+
+        return patientMapperService.mapToResponseDTO(savedPatient);
+    }
+
+    // Fetch all patient
+    public List<PatientResponseDTO> getAllPatients(){
+
+        return patientRepository.findAll()
+                .stream()
+                .map(patientMapperService::mapToResponseDTO)
+                .toList();
+    }
+
+    // Fetch patient by ID
+    public PatientResponseDTO getPatientByID(Long id){
+
+        return patientRepository.findById(id)
+                .map(patientMapperService::mapToResponseDTO)
+                .orElseThrow(() -> new ResourceNotFoundException("Patient not found !"));
+
+    }
+
+    // Update patient
+    public PatientResponseDTO updatePatient(Long id, PatientUpdateDTO updateDTO){
+
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Patient not found !"));
+
+        // Update only authorized field and if it's not null
+        if (updateDTO.getUsername() != null && !updateDTO.getUsername().isEmpty()){
+            patient.setUsername(updateDTO.getUsername());
+        }
+        if (updateDTO.getEmail() != null && !updateDTO.getEmail().isEmpty()){
+            patient.setEmail(updateDTO.getEmail());
+        }
+        if (updateDTO.getPassword() != null && !updateDTO.getPassword().isEmpty()){
+            patient.setPassword(passwordEncoder.encode(updateDTO.getPassword()));
+        }
+        if (updateDTO.getWeight() != null && updateDTO.getWeight() > 0){
+            patient.setWeight(updateDTO.getWeight());
+        }
+        if (updateDTO.getRegion() != null && !updateDTO.getRegion().isEmpty()){
+            patient.setRegion(updateDTO.getRegion());
+        }
+        if (updateDTO.getCity() != null && !updateDTO.getCity().isEmpty()){
+            patient.setCity(updateDTO.getCity());
+        }
+
+        Patient updatePatient = patientRepository.save(patient);
+        return patientMapperService.mapToResponseDTO(updatePatient);
+
+    }
+
+    // Delete patient
+    public void deletePatient(Long id){
+        patientRepository.deleteById(id);
+    }
+
+}
