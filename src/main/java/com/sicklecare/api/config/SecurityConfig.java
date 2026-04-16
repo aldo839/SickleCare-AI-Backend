f922ac1bc89075a22effeafbde359eff44a108e0
YoungIT237 <ityoung742@gmail.com>
2026-04-16 09:48:02 +0100

    Implementation of Patient validation Repository

---
+++ src/main/java/com/sicklecare/api/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.sicklecare.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // To manage csrf security on requests
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return
+                httpSecurity
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(
+                                authorize ->
+                                        authorize
+                                                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/patients/register").permitAll()
+                                                .anyRequest().authenticated()
+                        ).build();
+    }
+
+    // To encode password
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    // To manage JWT
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
+}
