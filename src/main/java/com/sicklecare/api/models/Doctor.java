d916b78e4bd62305965b61a075b5cae9fffe588d
YoungIT237 <ityoung742@gmail.com>
2026-04-16 08:27:54 +0100

    Extension of User entity with data specific to Doctor as Doctor entity

---
+++ src/main/java/com/sicklecare/api/models/Doctor.java
@@ -0,0 +1,23 @@
+package com.sicklecare.api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "doctors")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Doctor extends User {
+
+    private String firstname;
+    private String lastname;
+    private String speciality;
+    private String region;
+    private String city;
+    private String hospital;
+
+}
