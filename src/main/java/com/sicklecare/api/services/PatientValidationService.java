f7c998877c19662bf4e908b40af58f9fcdf12fab
YoungIT237 <ityoung742@gmail.com>
2026-04-16 09:50:23 +0100

    Implementation of Patient validation service

---
+++ src/main/java/com/sicklecare/api/services/PatientValidationService.java
@@ -0,0 +1,45 @@
+package com.sicklecare.api.services;
+
+import com.sicklecare.api.models.PatientValidation;
+import com.sicklecare.api.models.User;
+import com.sicklecare.api.repository.PatientValidationRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class PatientValidationService{
+
+    private final PatientValidationRepository patientValidationRepository;
+    private final NotificationService notificationService;
+
+    public void save(User user) {
+
+        PatientValidation patientValidation = new PatientValidation();
+
+        patientValidation.setUser(user);
+        Instant creation = Instant.now();
+        patientValidation.setCreation(creation);
+        Instant expiration = creation.plus(10, ChronoUnit.MINUTES);
+        patientValidation.setExpiration(expiration);
+        Random random = new Random();
+        int randomInteger = random.nextInt(999999);
+        String code = String.format("%06d", randomInteger);
+        patientValidation.setCode(code);
+
+        patientValidationRepository.save(patientValidation);
+
+        try {
+            notificationService.send(patientValidation);
+        } catch (Exception e) {
+            System.err.println("Problem to send email : " + e.getMessage());
+        }
+
+    }
+}
