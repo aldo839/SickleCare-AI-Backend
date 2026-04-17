83e72ebf0ad3641178a7373286a056f344ee9a25
YoungIT237 <ityoung742@gmail.com>
2026-04-17 23:20:33 +0100

    Modification of login request to use Map

--- src/main/java/com/sicklecare/api/controllers/AuthController.java
+++ src/main/java/com/sicklecare/api/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.sicklecare.api.controllers;
 
+import com.sicklecare.api.config.JwtUtils;
 import com.sicklecare.api.dtos.LoginRequestDTO;
+import com.sicklecare.api.repository.UserRepository;
 import com.sicklecare.api.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+    private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequestDTO loginDTO){
+    public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginRequestDTO loginDTO){
 
         return ResponseEntity.ok(authService.authenticate(loginDTO));
     }
