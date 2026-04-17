c8c5f2790b06b8c19986ff82bcc934ab5f3a1722
YoungIT237 <ityoung742@gmail.com>
2026-04-17 23:27:29 +0100

    Implementation of jwt token service for authentication of our users

--- src/main/java/com/sicklecare/api/services/AuthService.java
+++ src/main/java/com/sicklecare/api/services/AuthService.java
@@ -1,5 +1,6 @@
 package com.sicklecare.api.services;
 
+import com.sicklecare.api.config.JwtUtils;
 import com.sicklecare.api.dtos.LoginRequestDTO;
 import com.sicklecare.api.exceptions.BadCredentialsException;
 import com.sicklecare.api.exceptions.ResourceNotFoundException;
@@ -9,23 +10,35 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
 
-    public String authenticate(LoginRequestDTO loginDTO){
+    public Map<String, String> authenticate(LoginRequestDTO loginDTO) {
 
         User user = userRepository.findByEmail(loginDTO.getEmail())
                 .orElseThrow(() -> new ResourceNotFoundException("User not found !"));
 
-        if (!passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())){
+        if (!passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())) {
             throw new BadCredentialsException("Bad password !");
         }
 
-        return "Connection successful for " + user.getUsername();
-    }
+        if (!user.getActivated()) {
+            throw new RuntimeException("Unactivated account. Please verify your mails.");
+        }
 
+        String token = jwtUtils.generateToken(user);
+
+        return Map.of(
+                "token", token,
+                "username", user.getUsername(),
+                "role", user.getRole().name()
+        );
+    }
 }
