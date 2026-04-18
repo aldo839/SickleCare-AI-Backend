2f4a0887b5707b7b3341d17079787cc717b83562
YoungIT237 <ityoung742@gmail.com>
2026-04-18 18:17:03 +0100

    Define of DoctorResponseDTO

--- src/main/java/com/sicklecare/api/dtos/DoctorResponseDTO.java
+++ src/main/java/com/sicklecare/api/dtos/DoctorResponseDTO.java
@@ -1,11 +1,14 @@
 package com.sicklecare.api.dtos;
 
+import com.sicklecare.api.models.Role;
+
 public record DoctorResponseDTO(
 
         Long id,
         String username,
         String email,
-        String role,
+        String name,
+        Role role,
         String speciality,
         String matricule,
         String validationLetterReference,
