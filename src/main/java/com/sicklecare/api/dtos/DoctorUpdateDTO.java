c91a9adac5a8ba99ae5a152fe7d0a0308eb97366
YoungIT237 <ityoung742@gmail.com>
2026-04-18 17:40:09 +0100

    Define DoctorUpdateDTO

---
+++ src/main/java/com/sicklecare/api/dtos/DoctorUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.sicklecare.api.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public class DoctorUpdateDTO {
+
+    @Size(min = 4, max = 20, message = "Username will have between 4 and 20 characters !")
+    private String username;
+    @Email(message = "Email will be valid !")
+    private String email;
+    private String password;
+    private String hospitalUnit;
+    private String region;
+    private String city;
+    private Double hospital;
+
+}
