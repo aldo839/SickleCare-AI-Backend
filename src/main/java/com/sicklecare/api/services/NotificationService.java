e49ab61e2dda35f8881062287b4f6f0c6df55282
YoungIT237 <ityoung742@gmail.com>
2026-04-19 07:29:32 +0100

    Simple space adding

--- src/main/java/com/sicklecare/api/services/NotificationService.java
+++ src/main/java/com/sicklecare/api/services/NotificationService.java
@@ -1,6 +1,6 @@
 package com.sicklecare.api.services;
 
-import com.sicklecare.api.models.PatientValidation;
+import com.sicklecare.api.models.DoctorValidation;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,25 +13,26 @@ public class NotificationService {
 
     private final JavaMailSender javaMailSender;
 
-    public void send(PatientValidation patientValidation) {
+    public void send(DoctorValidation doctorValidation) {
 
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom("noreply@sicklecareai.ai");
-            message.setTo(patientValidation.getUser().getEmail());
+            message.setTo(doctorValidation.getUser().getEmail());
             message.setSubject("Your activation code");
 
             String text = String.format(
                     "Hello %s, \nyour activation code is %s. \nSee you soon",
-                    patientValidation.getUser().getUsername(),
-                    patientValidation.getCode()
+                    doctorValidation.getUser().getUsername(),
+                    doctorValidation.getCode()
             );
             message.setText(text);
 
             javaMailSender.send(message);
+
         } catch (MailException e) {
             System.out.println("Error on email sending process : " + e.getMessage());
-            System.out.println("Test code : " + patientValidation.getCode());
+            System.out.println("Test code : " + doctorValidation.getCode());
         }
 
     }
