e7cf7b4b3b4a352e6406aa136e9f4492cdd62cc9
YoungIT237 <ityoung742@gmail.com>
2026-04-19 14:49:08 +0100

    Modification of NotificationService to be used both by DoctorValidation and PatientValidation

--- src/main/java/com/sicklecare/api/services/NotificationService.java
+++ src/main/java/com/sicklecare/api/services/NotificationService.java
@@ -1,6 +1,8 @@
 package com.sicklecare.api.services;
 
 import com.sicklecare.api.models.DoctorValidation;
+import com.sicklecare.api.models.PatientValidation;
+import com.sicklecare.api.models.Validation;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,28 +15,25 @@ public class NotificationService {
 
     private final JavaMailSender javaMailSender;
 
-    public void send(DoctorValidation doctorValidation) {
-
+    public void send(Validation validation) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom("noreply@sicklecareai.ai");
-            message.setTo(doctorValidation.getUser().getEmail());
+            message.setTo(validation.getUser().getEmail());
             message.setSubject("Your activation code");
-
             String text = String.format(
-                    "Hello %s, \nyour activation code is %s. \nSee you soon",
-                    doctorValidation.getUser().getUsername(),
-                    doctorValidation.getCode()
+                    "Hello %s, \nyour activation code is %s \n See you on the application !",
+                    validation.getUser().getUsername(),
+                    validation.getCode()
             );
             message.setText(text);
 
             javaMailSender.send(message);
 
         } catch (MailException e) {
-            System.out.println("Error on email sending process : " + e.getMessage());
-            System.out.println("Test code : " + doctorValidation.getCode());
+            System.err.println("Error: " + e.getMessage());
+            System.out.println("Debug code: " + validation.getCode());
         }
-
     }
 
 }
