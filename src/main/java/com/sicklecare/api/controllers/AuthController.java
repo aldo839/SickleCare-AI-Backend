bc090613a193ae30de60d06f154900ec75baf6b2
YoungIT237 <ityoung742@gmail.com>
2026-04-24 15:36:46 +0100

    Adding of "registerPatient" and "registerDoctor" to centralize login and registration

--- src/main/java/com/sicklecare/api/controllers/AuthController.java
+++ src/main/java/com/sicklecare/api/controllers/AuthController.java
@@ -1,18 +1,20 @@
 package com.sicklecare.api.controllers;
 
-import com.sicklecare.api.config.JwtUtils;
-import com.sicklecare.api.dtos.LoginRequestDTO;
-import com.sicklecare.api.repository.UserRepository;
+import com.sicklecare.api.dtos.*;
 import com.sicklecare.api.services.AuthService;
+import com.sicklecare.api.services.DoctorService;
+import com.sicklecare.api.services.PatientService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,11 +22,25 @@ import java.util.Map;
 public class AuthController {
 
     private final AuthService authService;
+    private final PatientService patientService;
+    private final DoctorService doctorService;
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginRequestDTO loginDTO){
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginDTO){
 
         return ResponseEntity.ok(authService.authenticate(loginDTO));
+    }
+
+    @PostMapping("/register/patient")
+    public ResponseEntity<PatientResponseDTO> registerPatient(@Valid @RequestBody PatientRegistrationDTO patientRegistrationDTO) throws MessagingException, UnsupportedEncodingException {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientService.registerPatient(patientRegistrationDTO));
+    }
+
+    @PostMapping("/register/doctor")
+    public ResponseEntity<DoctorResponseDTO> registerDoctor(@Valid @RequestBody DoctorRegistrationDTO doctorRegistrationDTO) throws MessagingException, UnsupportedEncodingException {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.registerDoctor(doctorRegistrationDTO));
     }
 
 }
