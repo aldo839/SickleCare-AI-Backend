b24584923100450907f9b1ae544ec70101c58e2d
YoungIT237 <ityoung742@gmail.com>
2026-04-24 15:30:33 +0100

    Modification of AuthService to return LoginResponseDTO instead of a Map. Implementation of data masking: firstname and lastname are now hidden for Patients to ensure privacy.

--- src/main/java/com/sicklecare/api/services/AuthService.java
+++ src/main/java/com/sicklecare/api/services/AuthService.java
@@ -2,8 +2,11 @@ package com.sicklecare.api.services;
 
 import com.sicklecare.api.config.JwtUtils;
 import com.sicklecare.api.dtos.LoginRequestDTO;
+import com.sicklecare.api.dtos.LoginResponseDTO;
 import com.sicklecare.api.exceptions.BadCredentialsException;
 import com.sicklecare.api.exceptions.ResourceNotFoundException;
+import com.sicklecare.api.models.Admin;
+import com.sicklecare.api.models.Doctor;
 import com.sicklecare.api.models.User;
 import com.sicklecare.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +23,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
 
-    public Map<String, String> authenticate(LoginRequestDTO loginDTO) {
+    public LoginResponseDTO authenticate(LoginRequestDTO loginDTO) {
 
         User user = userRepository.findByEmail(loginDTO.getEmail())
                 .orElseThrow(() -> new ResourceNotFoundException("User not found !"));
@@ -35,10 +38,24 @@ public class AuthService {
 
         String token = jwtUtils.generateToken(user);
 
-        return Map.of(
-                "token", token,
-                "username", user.getUsername(),
-                "role", user.getRole().name()
+        // For patient
+        String firstname = null;
+        String lastname = null;
+
+        if (user instanceof Admin admin){
+            firstname = admin.getFirstname();
+            lastname = admin.getLastname();
+        } else if (user instanceof Doctor doctor){
+            firstname = doctor.getFirstname();
+            lastname = doctor.getLastname();
+        }
+
+        return new LoginResponseDTO(
+                token,
+                user.getUsername(),
+                user.getRole().name(),
+                firstname,
+                lastname
         );
     }
 }
