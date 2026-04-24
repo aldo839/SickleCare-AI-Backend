56c0c8a4b2a9d3577192bfa89323cc64c5087357
YoungIT237 <ityoung742@gmail.com>
2026-04-24 07:35:07 +0100

    Adding of validation method on variables of AdminRegistrationDTO

--- src/main/java/com/sicklecare/api/dtos/AdminRegistrationDTO.java
+++ src/main/java/com/sicklecare/api/dtos/AdminRegistrationDTO.java
@@ -10,16 +10,18 @@ import lombok.Setter;
 @Setter
 public class AdminRegistrationDTO {
 
-    @NotBlank(message = "Username is require !")
-    @Size(min = 5, max = 20, message = "Username may have between 5 and 20 characters !")
+    @NotBlank(message = "Username is required !")
+    @Size(min = 5, max = 20, message = "Username must be between 5 and 20 characters !")
     private String userName;
     @Email(message = "Email should be valid !")
-    @NotBlank(message = "Email is require !")
+    @NotBlank(message = "Email is required !")
     private String email;
-    @NotBlank(message = "Password is require !")
-    @Size(min = 5, max = 20, message = "Username may have between 8 and 20 characters !")
+    @NotBlank(message = "Password is required !")
+    @Size(min = 5, max = 20, message = "Password must be between 8 and 20 characters !")
     private String password;
+    @NotBlank(message = "Firstname is required!")
     private String firstname;
+    @NotBlank(message = "Lastname is required!")
     private String lastname;
 
 }
