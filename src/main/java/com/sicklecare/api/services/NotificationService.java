48c537de75392c9f413f40d9d06534c105406e11
YoungIT237 <ityoung742@gmail.com>
2026-04-27 21:24:50 +0100

    Modification of a method "send" by function "sendActivationCode" Implementation of a method sendAdminValidationSuccess to send a message to a patient or a doctor went the administrator will verify information and validate his account

--- src/main/java/com/sicklecare/api/services/NotificationService.java
+++ src/main/java/com/sicklecare/api/services/NotificationService.java
@@ -2,11 +2,13 @@ package com.sicklecare.api.services;
 
 import com.sicklecare.api.models.DoctorValidation;
 import com.sicklecare.api.models.PatientValidation;
+import com.sicklecare.api.models.User;
 import com.sicklecare.api.models.Validation;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +17,7 @@ public class NotificationService {
 
     private final JavaMailSender javaMailSender;
 
-    public void send(Validation validation) {
+    public void sendActivationCode(Validation validation) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom("noreply@sicklecareai.ai");
@@ -33,6 +35,27 @@ public class NotificationService {
         } catch (MailException e) {
             System.err.println("Error: " + e.getMessage());
             System.out.println("Debug code: " + validation.getCode());
+        }
+    }
+
+    @Async
+    public void sendAdminValidationSuccess(User user) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("noreply@sicklecareai.ai");
+            message.setTo(user.getEmail());
+            message.setSubject("Account Validated - SickleCare");
+
+            String text = String.format(
+                    "Hello %s, \nYour account has been officially validated by our administration. " +
+                            "You can now access all features of SickleCare AI.",
+                    user.getUsername()
+            );
+            message.setText(text);
+            javaMailSender.send(message);
+
+        } catch (MailException e) {
+            System.err.println("SMTP Error (Admin Validation): " + e.getMessage());
         }
     }
 
