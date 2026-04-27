b34f514d251ee966a67d4392ec8df39837add5fa
YoungIT237 <ityoung742@gmail.com>
2026-04-27 18:42:50 +0100

    Adding of method findByDoctorId to see all patients of one doctor, and method existsByIdAndDoctorId to check a doctor of one patient.

--- src/main/java/com/sicklecare/api/repository/PatientRepository.java
+++ src/main/java/com/sicklecare/api/repository/PatientRepository.java
@@ -1,20 +1,25 @@
 package com.sicklecare.api.repository;
 
 import com.sicklecare.api.models.Patient;
+import com.sicklecare.api.models.PatientSex;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-    List<Patient> findBySex(Boolean sex);
+    List<Patient> findBySex(PatientSex sex);
 
     List<Patient> findByRegion(String region);
 
     List<Patient> findByCity(String city);
 
-    List<Patient> findByValidatedTrue(Boolean validated);
+    List<Patient> findByValidatedTrue();
 
     List<Patient> findByGenotype(String genotype);
+
+    List<Patient> findByDoctorId(Long doctorId);
+
+    Boolean existsByIdAndDoctorId(Long patientId, Long doctorId);
 
 }
